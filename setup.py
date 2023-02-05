354db5f0856b0ff8bdfb065867aac88d784b92c1
Eduardo Willians <78479953+eduardo-willians@users.noreply.github.com>
2023-02-05 16:13:37 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='kwadro',
     author='Eduardo Willians Bandeira',
-    url = "https://github.com/eduardo-willians/kwadro",
+    url="https://github.com/eduardo-willians/kwadro",
     license='MIT',
-    install_requires = ['openpyxl'],
+    install_requires=['openpyxl'],
+    version="0.1",
     scripts=['kwadro.py'])
