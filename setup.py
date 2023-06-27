2d07c535c763cbcbb0e3184a37f5f97e539831e9
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2023-06-27 05:43:29 -0700

    Cosmetic changes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='kwadro',
     version="0.2",
-    author='Eduardo Willians Bandeira',
-    url="https://github.com/eduardo-willians/kwadro",
+    author='Eduardo Bandeira',
+    url="https://github.com/eduardo-w-bandeira/kwadro",
     license='MIT',
     install_requires=['openpyxl'],
     scripts=['kwadro.py'])
